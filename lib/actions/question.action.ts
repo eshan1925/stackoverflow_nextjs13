e59877a158459b64a1d2c77f26a8a7534677effe
eshan1925 <eshan@getmedesign.com>
2024-02-10 00:16:21 +0530

    Finished the functionality of Questions details page with answering func

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -3,26 +3,26 @@
 import Question from "@/database/question.model";
 import Tag from "@/database/tag.model";
 import { connectToDatabase } from "../mongoose";
-import { CreateQuestionParams, GetQuestionsParams } from "./shared.types";
+import { CreateQuestionParams, GetQuestionByIdParams, GetQuestionsParams } from "./shared.types";
 import User from "@/database/user.model";
 import { revalidatePath } from "next/cache";
 
-export async function getQuestions(params:GetQuestionsParams){
+export async function getQuestions(params: GetQuestionsParams) {
   try {
     connectToDatabase();
     const questions = await Question.find({})
-    .populate({path:'tags',model:Tag})
-    .populate({path:'author',model:User})
-    .sort({createdAt:-1})
+      .populate({ path: "tags", model: Tag })
+      .populate({ path: "author", model: User })
+      .sort({ createdAt: -1 });
 
-    return{questions};
+    return { questions };
   } catch (error) {
     console.log(error);
     throw error;
   }
 }
 
-export async function createQuestion(params:CreateQuestionParams) {
+export async function createQuestion(params: CreateQuestionParams) {
   "use server";
   try {
     connectToDatabase();
@@ -55,4 +55,25 @@ export async function createQuestion(params:CreateQuestionParams) {
     console.log(path);
     revalidatePath(path);
   } catch (error) {}
+}
+
+export async function getQuestionById(params: GetQuestionByIdParams) {
+  try {
+    connectToDatabase();
+    const { questionId } = params;
+    const question = await Question.findById(questionId).populate({
+      path: "tags",
+      model: Tag,
+      select: "_id name",
+    }).populate({
+      path: "author",
+      model: User,
+      select: "_id clerkId name picture",
+    })
+
+    return question;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
